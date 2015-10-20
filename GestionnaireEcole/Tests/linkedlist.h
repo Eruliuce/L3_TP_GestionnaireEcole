2131969829854f3958d8f49297272f3e2301e891
Eruliuce <arnaudbriant@hotmail.fr>
2015-10-20 10:10:42 +0200

    Update linkedlist.h

--- GestionnaireEcole/Tests/linkedlist.h
+++ GestionnaireEcole/Tests/linkedlist.h
@@ -3,11 +3,11 @@
 ///     test_linkedlist.h                           ///
 ///                                                 ///
 /// Auteurs : RIBEIRO Olivier & BRIANT Arnaud       ///
-/// Création : 17/09/2015                           ///
-/// Dernière modification : 17/09/2015              ///
+/// CrÃ©ation : 17/09/2015                           ///
+/// DerniÃ¨re modification : 20/10/2015              ///
 ///                                                 ///
-/// Fonction : Test des fonctionnalités de la       ///
-///            liste chaînée.                       ///
+/// Fonction : Test des fonctionnalitÃ©s de la       ///
+///            liste chaÃ®nÃ©e.                       ///
 ///                                                 ///
 /// ///////////////////////////////////////////////////
 
@@ -17,17 +17,21 @@
 #include <stdlib.h>
 #include "..\Outils\linkedlist.h"
 
-void testLinkedList(); //éxecute une série de tests pour vérifier que la LinkedList fonctionne bien
+void testLinkedList(); //Ã©xecute une sÃ©rie de tests pour vÃ©rifier que la LinkedList fonctionne bien
 
-size_t testPremierMaillon(); //retourne 0 si le premier maillon se crée correctement, 1 sinon
-size_t testXMaillons(size_t x); //retourne 0 si tous les maillons se créent correctement, une autre valeur sinon
+size_t testPremierMaillon(); //retourne 0 si le premier maillon se crÃ©e correctement, 1 sinon
+size_t testXMaillons(size_t x); //retourne 0 si tous les maillons se crÃ©ent correctement, une autre valeur sinon
 size_t testPullUnMaillon();
 size_t testPullXMaillons(size_t x);
 size_t testSuppression();
 void testExec();
 void testExecAfficheInt(int *i);
 size_t triNumeral(size_t *a, size_t *b);
+size_t comparaisonNum(size_t *a, size_t b);
 void testInsert();
 size_t testSize();
+size_t testRemove();
+size_t testGet();
 
 #endif // TEST_LINKEDLIST_H_INCLUDED
+
